1bfe4f4f42f70879d2b6e5ba8574a4296a9c219e
Godspower Similoluwa <gpsimi01@gmail.com>
2024-04-08 19:03:10 +0100

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <div className="flex text-center items-center justify-center">
+      <h5 className="text-3xl font-bold text-purple-900">
+        Welcome to <span className="text-yellow-500">Spinet Digital Agency!</span> 
+      </h5>
+    </div>
+  )
+}
+
+export default App
